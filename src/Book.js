dcac19792d70aa3158c02507b05f7a0c37a4392e
matsvandamme <matsvandamme@gmail.com>
2021-04-04 23:57:28 +0700

    ok

---
+++ src/Book.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+
+class Book extends React.Component {
+
+    handleShelfChange= (event) => {
+        this.props.shelfChange(this.props.book,event.target.value);
+      }
+    
+    render() {
+        return <div className="book">
+        <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+        <div className="book-shelf-changer">
+            <select onChange={this.handleShelfChange} value={this.props.book.shelf}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none" selected>None</option>
+            </select>
+        </div>
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors.join(', ')}</div>
+        </div>;
+    }}
+  
+
+  export default Book; 
---
+++ src/Bookshelf.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import Book from './Book.js'
+
+class Bookshelf extends React.Component {
+
+    render() {
+      return(
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">{this.props.title}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {this.props.books.map((book)=>{
+                      return <li key={book.title}><Book book={book} shelfChange={this.props.shelfChange}/></li>
+                    })}
+                </ol>
+              </div>
+            </div>
+    );
+    }}
+  
+
+  export default Bookshelf; 
